695ce5a5090c0432a1c4f6cdd15a1c8a0e400ba9
ryan <qq446736049@yahoo.com>
2022-08-25 22:06:45 -0400

    1

--- config.js
+++ config.js
@@ -30,7 +30,7 @@ var config = {
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
-        turn_on: "t3",
+        turn_on: "t4",
         play: "     tttt     ",
         bannar_coming: "颜色",
         balloons_flying: "好像少点东西",
