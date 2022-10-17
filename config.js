5ad6347cc1bb158bf9e6d158af2a4bc58463c764
ryan <qq446736049@yahoo.com>
2022-10-16 20:21:20 -0400

    Update config.js

--- config.js
+++ config.js
@@ -30,9 +30,9 @@ var config = {
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
-        turn_on: "t4",
-        play: "     tttt     ",
-        bannar_coming: "颜色",
+        turn_on: "Click Me",
+        play: "Play",
+        bannar_coming: "生日快乐",
         balloons_flying: "好像少点东西",
         cake_fadein: "蛋糕？",
         light_candle: "蜡烛？",
